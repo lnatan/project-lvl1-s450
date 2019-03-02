6cb39bb4a7320ca9071fa88c4633ef02d9e16053
nata le <lna29ster@gmail.com>
2019-03-02 11:38:52 +0300

    add brain-progression

---
+++ src/games/progression.js
@@ -0,0 +1,32 @@
+import randomNumFrom from '../utils';
+import runGame from '..';
+
+const makeProgression = (start, step, length) => {
+  const arr = [];
+
+  for (let i = 0; i < length; i += 1) {
+    arr.push(start + step * i);
+  }
+
+  return arr;
+};
+
+const task = 'What number is missing in the progression?';
+
+const brainProgression = () => {
+  const start = randomNumFrom(0, 100);
+  const step = randomNumFrom(1, 20);
+  const progression = makeProgression(start, step, 10);
+
+  const hiddenNumIndex = randomNumFrom(0, progression.length - 1);
+
+  const progressionWithHiddenNum = [...progression];
+  progressionWithHiddenNum[hiddenNumIndex] = '..';
+
+  const question = progressionWithHiddenNum.join(' ');
+  const expected = String(progression[hiddenNumIndex]);
+
+  return { question, expected };
+};
+
+export default () => runGame(task, brainProgression);
